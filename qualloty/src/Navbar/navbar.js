affe8a7094cad0850841fc56ded2242fa8e3f1b1
Jeffrey Wang <jwan2369@uwo.ca>
2022-04-16 14:59:06 -0400

    added dark mode button, only toggles nav bar color

--- qualloty/src/Navbar/navbar.js
+++ qualloty/src/Navbar/navbar.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './navbar.css';
 
+const black = '#000000';
+const purple = '#4C367A';
+
 export class NavBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {darkmode: false, color: '#4C367A'};
+    this.toggleDark = this.toggleDark.bind(this);
+  }
+  toggleDark(){
+    if (!this.state.darkmode){
+      this.setState({darkmode: true, color:black});
+    }
+    else{
+      this.setState({darkmode: false, color: purple});
+    }
+  }
+
   render() {
-    const pages = ['☾','Sign Up', 'Login'];
+    const pages = ['Sign Up', 'Login'];
     const navLinks = pages.map(page => {
       return (
         <a class="page" href={'/' + page}>
@@ -13,9 +30,13 @@ export class NavBar extends React.Component {
     });
 
     return (
-      <div className="header">
+      <div className="header" style={{background: this.state.color}}>
         <h1>Qualloty</h1>
-        <nav class="navLinks">{navLinks}</nav>
+        
+        <nav class="navLinks">
+          <button onClick={this.toggleDark}>☾</button>
+          {navLinks}
+        </nav>
       </div>
     );
   }
