1103b6d2d6b408d366f8cef4bb8bd07e3caf6d2d
Benjamin Critoph <benjamincritoph@Benjamins-MacBook-Air-2.local>
2022-04-24 21:29:37 -0400

    before pull

---
+++ qualloty/src/components/MainMenu/MainMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './MainMenu.module.css';
+
+const MainMenu = () => (
+  <div className={styles.MainMenu}>
+    MainMenu Component
+  </div>
+);
+
+MainMenu.propTypes = {};
+
+MainMenu.defaultProps = {};
+
+export default MainMenu;
